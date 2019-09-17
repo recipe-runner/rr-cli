8d237aa87f1f50c0abe25e8ebb5a55a1f5a9e4fe
yosymfony <yosymfony@gmail.com>
2019-09-17 15:04:13 +0000

    Version fiexed to 1.0.0-alpha1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,6 @@
 CHANGELOG
 =========
+
+## 1.0.0-alpha1 (2019-09-17)
+
+* Initial release.
--- src/CliInterface/Application.php
+++ src/CliInterface/Application.php
@@ -21,7 +21,7 @@ use Symfony\Component\Console\Application as BaseApplication;
  */
 class Application extends BaseApplication
 {
-    const VERSION = '0.0.1';
+    const VERSION = '1.0.0-alpha1';
 
     private static $logo = '
     ____     ___    __  ____  ____   ___      ____  __ __  ____   ____     ___  ____      
